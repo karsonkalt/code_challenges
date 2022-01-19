8438eaf8f5bc193fd126dcdaf443a166c5cbd59c
Karson <kars.kalt@gmail.com>
2022-01-19 09:49:51 -0500

    Added memoization

--- longestStringChain.js
+++ longestStringChain.js
@@ -1,20 +1,24 @@
 var longestStrChain = function (words) {
-  // let cache = {}
+  let cache = {};
 
   function rm(str, i) {
     return str.substr(0, i) + str.substr(i + 1);
   }
 
   function recurse(word) {
-    // if (cache.has(word)) return cache[word]
+    if (word in cache) return cache[word];
+    let maxLength = 1;
+
     console.log(word);
     for (let i = 0; i < word.length; i++) {
       let nw = rm(word, i);
       if (words.includes(nw)) {
-        return 1 + recurse(nw);
+        let curLength = 1 + recurse(nw);
+        maxLength = Math.max(maxLength, curLength);
       }
     }
-    return 1;
+    cache[word] = maxLength;
+    return maxLength;
     //make all the words available
     // if that word is in words increase length by one and do it again
   }
@@ -23,10 +27,12 @@ var longestStrChain = function (words) {
 
   for (const word of words) {
     length.push(recurse(word));
+    console.log(cache);
     console.log("----");
   }
 
   console.log(length);
+  console.log(cache);
 
   return Math.max(...length);
 };
